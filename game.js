3570fda20933d45fa9aa2958ef4ade8c913c3b08
Matheus Rocha <01devmath@gmail.com>
2022-03-11 12:06:42 -0300

    Add settings UI, Update create grid with javascript

---
+++ game.js
@@ -0,0 +1,24 @@
+const colorInput = document.getElementById("color-input");
+const grid = document.querySelector(".game-grid");
+const gridSizeInput = document.getElementById("gridsize-input");
+const resetBtn = document.getElementById("reset-btn");
+
+let currentColor = "#000000";
+let currentGridSize = "16x16";
+
+function createGrid() {
+  for (let index = 0; index < 16 * 16; index++) {
+    grid.style.gridTemplateRows = `repeat(16,1fr)`;
+    grid.style.gridTemplateColumns = `repeat(16,1fr)`;
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("square");
+    gridSquare.addEventListener("mouseover", () => console.log('hoho'));
+    grid.appendChild(gridSquare);
+  }
+}
+
+function resetGame() {
+
+}
+
+createGrid()
