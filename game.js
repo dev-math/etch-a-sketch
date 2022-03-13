e87f9ba40e4fdf1727ab3d0268732b884f53d0d7
Matheus Rocha <01devmath@gmail.com>
2022-03-13 11:43:20 -0300

    Add function to reset drawing

--- game.js
+++ game.js
@@ -3,25 +3,36 @@ const grid = document.querySelector(".game-grid");
 const gridSizeInput = document.getElementById("gridsize-input");
 const resetBtn = document.getElementById("reset-btn");
 
-colorInput.onchange = () => resetGame();
-gridSizeInput.onchange = () => resetGame();
-resetBtn.onclick = () => resetGame();
+// colorInput.onchange = () => resetGame();
+// gridSizeInput.onchange = () => resetGame();
+resetBtn.onclick = () => resetDrawing();
 
 let currentColor = "#000000";
 let currentGridSize = "16x16";
 
+// temp
+let gridc = 16;
+let gridr = 16;
+let gridBackgroundColor = "#ffffff";
+
 function createGrid() {
   for (let index = 0; index < 16 * 16; index++) {
-    grid.style.gridTemplateRows = `repeat(16,1fr)`;
-    grid.style.gridTemplateColumns = `repeat(16,1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridr},1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${gridc},1fr)`;
     const gridSquare = document.createElement("div");
     gridSquare.classList.add("square");
-    gridSquare.addEventListener("mouseover", () => console.log('test'));
+    gridSquare.addEventListener("mouseover", () => gridSquare.style.background = "#f00");
     grid.appendChild(gridSquare);
   }
 }
 
-function resetGame() {
+function resetDrawing() {
+  const gridSquares = document.querySelectorAll('.square');
+  gridSquares.forEach(element => element.style.background = gridBackgroundColor);
+}
+
+function changeGrid() {
+  
 }
 
 createGrid()
