44336bbbba9a082f262049fb7000e7c0b0d7ca0e
Matheus Rocha <01devmath@gmail.com>
2022-03-14 19:56:29 -0300

    Add rainbow color mode

--- game.js
+++ game.js
@@ -2,13 +2,16 @@ const colorInput = document.getElementById("color-input");
 const grid = document.querySelector(".game-grid");
 const gridSizeInput = document.getElementById("gridsize-input");
 const resetBtn = document.getElementById("reset-btn");
+const rainbowBtn = document.getElementById("rainbow-btn");
 
-colorInput.onchange = () => gridForeground = colorInput.value;
-gridSizeInput.onchange = () => createGrid(gridSizeInput.value);
-resetBtn.onclick = () => resetDrawing();
-
+let currentMode = "static";
 let gridBackground = "#ffffff";
 let gridForeground = "#aaaaaa";
+
+gridSizeInput.onchange = () => createGrid(gridSizeInput.value);
+resetBtn.onclick = () => resetDrawing();
+rainbowBtn.onclick = () => rainbowMode();
+
 
 function createGrid(gridSize) {
   resetGrid();
@@ -27,12 +30,30 @@ function resetGrid() {
 }
 
 function resetDrawing() {
-  const gridSquares = document.querySelectorAll('.square');
+  const gridSquares = document.querySelectorAll(".square");
   gridSquares.forEach(element => element.style.background = gridBackground);
 }
 
 function paintSquare(square) {
+  if (currentMode == "rainbow") {
+    let randomRed = Math.floor(Math.random() * 256);
+    let randomGreen = Math.floor(Math.random() * 256);
+    let randomBlue = Math.floor(Math.random() * 256);
+    gridForeground = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+  } else if (currentMode == "static") {
+    gridForeground = colorInput.value;
+  }
   square.style.background = gridForeground;
+}
+
+function rainbowMode() {
+  if (currentMode == "rainbow") {
+    currentMode = "static";
+    rainbowBtn.classList.remove("active");
+  } else if (currentMode == "static") {
+    currentMode = "rainbow";
+    rainbowBtn.classList.add("active");
+  }
 }
 
 createGrid(16);
